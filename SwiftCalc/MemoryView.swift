c80bf92daa761ce491794fbf6b9070db592556d1
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-09-02 15:09:21 +0200

    initial commit

---
+++ SwiftCalc/MemoryView.swift
@@ -0,0 +1,32 @@
+//
+//  MemoryView.swift
+//  SwiftCalc
+//
+//  Created by Kirill Streltsov on 02.09.23.
+//
+
+import SwiftUI
+
+struct MemoryView: View {
+  @Binding var memory: Double
+  var geometry: GeometryProxy
+
+  var body: some View {
+    HStack {
+      Spacer()
+      Text("\(memory)")
+        .padding(.horizontal, 5)
+        .frame(
+          width: geometry.size.width * 0.85,
+          alignment: .trailing
+        )
+        .overlay(
+          RoundedRectangle(cornerRadius: 8)
+            .stroke(lineWidth: 2)
+            .foregroundColor(Color.gray)
+        )
+      // Add gesture here
+      Text("M")
+    }.padding(.bottom).padding(.horizontal, 5)
+  }
+}
