9dd775891e7620524d45e0b4cab3ab56ecf2f733
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-09-02 15:03:58 +0200

    Initial Commit

---
+++ SwiftCalc/SwiftCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCalcApp.swift
+//  SwiftCalc
+//
+//  Created by Kirill Streltsov on 02.09.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
